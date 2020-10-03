796ffbd50c98841cc70fbe669e694ef1dfce0e62
Rohit Goyal <irohitgoyal@gmail.com>
2020-10-03 12:19:05 +0530

    Update Test.java
    The variable should be long and not int

--- src/Test.java
+++ src/Test.java
@@ -3,9 +3,8 @@ public class Test {
 
 	public static void main(String[] args) {
 		
-		Long myInt= 6442450944L;
+		Long myLong= 6442450944L;
 		             
-		
 		System.out.println(Integer.MAX_VALUE);
 	}
 }
